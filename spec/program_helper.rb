be7a18b94fa76e4eddf526a708597b0cc92b708c
dniman <dade.niman@gmail.com>
2017-01-12 11:48:59 +0300

    program_helper as default

---
+++ spec/program_helper.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+require 'stringio'
+
+def capture(stream)
+  begin
+    stream = stream.to_s
+    eval "$#{stream} = StringIO.new"
+    yield
+    result = eval("$#{stream}").string
+  ensure
+    eval("$#{stream} = #{stream.upcase}")
+  end
+
+  result
+end
