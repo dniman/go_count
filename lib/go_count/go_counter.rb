c11954968227495e75265da2f30626839a10c50b
dniman <dade.niman@gmail.com>
2017-01-13 15:24:33 +0300

    directory is empty

--- lib/go_count/go_counter.rb
+++ lib/go_count/go_counter.rb
@@ -1,13 +1,23 @@
 module GoCount
   class GoCounter
     def count
-			Dir[File.expand_path('../../..',__FILE__) + "/data/*.sql"].each do |file_name| 
+			puts "Directory is empty" if files.count <= 0
+			files.each do |file_name| 
 	      go_count = 0
   		  File.open(file_name).each do |line|
 	  		  go_count += 1 if line.match(/GO\n/)
         end
         puts "#{file_name} - #{go_count}"
 		  end
+    end
+
+		private
+		def path
+			File.expand_path('../../../data',__FILE__)		  
+    end
+
+    def files
+		  Dir["#{path}/*.sql"]
     end
   end
 end
--- spec/go_counter_spec.rb
+++ spec/go_counter_spec.rb
@@ -1,8 +1,24 @@
+require 'fileutils'
 module GoCount
   RSpec.describe GoCounter do
 		describe "#count" do
-		  it "sends file name with go count" do
-			  expect(capture(:stdout){ GoCounter.new.count }).to match(/\S*\.sql\s-\s\d*/)
+			let(:program) { GoCounter.new }
+		  before :each do
+				Dir[File.expand_path('../../data/*.sql',__FILE__)].each{|f| FileUtils.remove(f) }					        
+      end
+
+      context "when path is empty" do
+        it "sends directory is empty" do
+          expect(capture(:stdout){program.count}).to match(/Directory is empty/)
+        end
+      end
+
+      context "when path is not empty" do
+		  	it "sends file name with go count" do
+          File.write(File.expand_path('../../data/test.sql', __FILE__), '') 
+
+			  	expect(capture(:stdout){ program.count }).to match(/\S*\.sql\s-\s\d*/)
+				end
 			end
     end
   end
